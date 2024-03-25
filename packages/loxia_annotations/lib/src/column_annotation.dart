689df02a9424fd53183e9ffa91c22b3244e8dfc2
Francesco Vallone <francesco.vallone@orbyta.it>
2024-03-25 17:57:42 +0100

    feat: start relations implementation

--- packages/loxia_annotations/lib/src/column_annotation.dart
+++ packages/loxia_annotations/lib/src/column_annotation.dart
@@ -48,6 +48,60 @@ class PrimaryKey extends Column{
     super.name,
     this.autoIncrement = false,
     this.uuid = false,
+  }): assert(autoIncrement == false || uuid == false, 'autoIncrement and uuid cannot be true at the same time');
+
+}
+
+abstract class Relation{
+
+  final dynamic Function()? on;
+  final dynamic Function(dynamic)? to;
+  final bool hasForeignKey;
+
+  const Relation({
+    this.on,
+    this.to,
+    this.hasForeignKey = false,
+  });
+
+}
+
+class OneToOne extends Relation{
+
+  const OneToOne({
+    super.on,
+    super.to,
+    super.hasForeignKey = false,
+  });
+
+}
+
+class OneToMany extends Relation{
+
+  const OneToMany({
+    super.on,
+    super.to,
+    super.hasForeignKey = false,
+  });
+
+}
+
+class ManyToOne extends Relation{
+
+  const ManyToOne({
+    super.on,
+    super.to,
+    super.hasForeignKey = false,
+  });
+
+}
+
+class ManyToMany extends Relation{
+
+  const ManyToMany({
+    super.on,
+    super.to,
+    super.hasForeignKey = false,
   });
 
 }
